6723c97d45ff3ec80ca7a5b6370efb1aaba4f1e3
Ubuntu <ubuntu@ip-172-31-39-181.ap-northeast-1.compute.internal>
2020-07-13 06:10:11 +0000

    second commit

---
+++ 8/8-12/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.name = "ミラノ";
+        h.hp = 100;
+        System.out.println("イタリア" + h.name + "地方");
+        
+    }
+}
